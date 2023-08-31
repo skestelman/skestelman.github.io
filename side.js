56158018e8f77c658e36edc357a249944cbf8667
Steph <stephaniekestelman@gmail.com>
2023-08-31 14:33:36 -0400

    Update CV

--- side.js
+++ side.js
@@ -5,7 +5,7 @@ document.write('\
   <img src="image/skestelm_circle.png" width=230 height=230 alt="Stephanie Kestelman profile picture">\
         <h4><p class="view"><a href="https://skestelman.github.io/">About Me</a></p></h4>\
         <h4><p class="view"><a href="https://skestelman.github.io/research.html">Research</a></p></h4>\
-  <h4><p class="view"><a href="https://skestelman.github.io/cv/SKestelman_CV_oct2021.pdf">CV</a></p></h4>\
+  <h4><p class="view"><a href="https://skestelman.github.io/cv/SKestelman_CV_2023aug.pdf">CV</a></p></h4>\
   <h4><p class="view"><a href="https://skestelman.github.io/data_programs.html">Coding and Codes</a></p></h4>\
   <h4><p class="view"><a href="https://skestelman.github.io/diversityecon.html">Diversity in Economics</a></p></h4>\
   <p class="view"><a href="https://twitter.com/stephkestelman"> Twitter </a> </p>   \
