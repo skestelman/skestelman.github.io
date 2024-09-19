34d94f8ea218b831d025f509aca77f993618d79d
Stephanie Kestelman <stephaniekestelman@gmail.com>
2024-09-19 14:15:35 -0400

    Update side.js

--- side.js
+++ side.js
@@ -2,7 +2,7 @@ document.write('\
 \
 	<header>\
   <h2><p class="view"><a href="https://skestelman.github.io/">Stephanie Kestelman</a></p></h2>\
-  <img src="image/stephaniekestelman_photo_circ.png" width=230 height=230 alt="Stephanie Kestelman profile picture">\
+  <img src="image/stephaniekestelman_photo_circ.jpg" width=230 height=230 alt="Stephanie Kestelman profile picture">\
         <h4><p class="view"><a href="https://skestelman.github.io/">About Me</a></p></h4>\
         <h4><p class="view"><a href="https://skestelman.github.io/research.html">Research</a></p></h4>\
   <h4><p class="view"><a href="cv/SKestelman_CV_2024may.pdf">CV</a></p></h4>\
