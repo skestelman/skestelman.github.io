e5d69f77c79f05c02fd6b5fe83c9cba5c2a73901
Stephanie Kestelman <stephaniekestelman@gmail.com>
2025-03-05 14:52:52 -0500

    Update side.js

--- side.js
+++ side.js
@@ -5,7 +5,7 @@ document.write('\
   <img src="image/stephaniekestelman_photo_circ.jpg" width=230 height=230 alt="Stephanie Kestelman profile picture">\
         <h4><p class="view"><a href="https://skestelman.github.io/">About Me</a></p></h4>\
         <h4><p class="view"><a href="https://skestelman.github.io/research.html">Research</a></p></h4>\
-  <h4><p class="view"><a href="cv/SKestelman_CV_2024may.pdf">CV</a></p></h4>\
+  <h4><p class="view"><a href="cv/SKestelman_CV_2025march.pdf">CV</a></p></h4>\
         <h4><p class="view"><a href="https://skestelman.github.io/teaching.html">Teaching</a></p></h4>\
   <p class="view"><a href="https://twitter.com/stephkestelman"> Twitter </a> </p>   \
   </header>\
