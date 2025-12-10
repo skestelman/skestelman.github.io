2e6d5034fc68a68f92cfc5f201b50b0daf7ce148
Steph <stephaniekestelman@gmail.com>
2025-12-09 23:47:21 -0500

    Update CV

--- side.js
+++ side.js
@@ -4,7 +4,7 @@ document.write('\
 <h2><p class="view"><a href="https://skestelman.github.io/">Stephanie Kestelman</a></p></h2>\
 <img src="image/stephaniekestelman_photo_circ.jpg" width=230 height=230 alt="Stephanie Kestelman profile picture">\
 <h4><p class="view"><a href="https://skestelman.github.io/">Home</a></p></h4>\
-<h4><p class="view"><a href="cv/SKestelman_CV_2025oct.pdf">CV</a></p></h4>\
+<h4><p class="view"><a href="cv/SKestelman_CV_2025dec.pdf">CV</a></p></h4>\
 <h4><p class="view"><a href="https://skestelman.github.io/teaching.html">Teaching</a></p></h4>\
 <h4><p class="view"><a href="https://skestelman.github.io/dexter.html">My dog, Dexter</a></p></h4>\
   </header>\
